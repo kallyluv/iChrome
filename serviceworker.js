96d03d5587d031388b9947ac35bbdc6bb9ff7e00
xJustJqy <78117567+xJustJqy@users.noreply.github.com>
2021-06-03 12:31:07 -0700

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,7 @@
+version = 1.2;
 self.addEventListener("activate",event => {
   event.waitUntil(caches.keys().then(versions => Promise.all(versions.map(cache => {
-    if (cache != 1.1) return caches.delete(cache);
+    if (cache != version) return caches.delete(cache);
   }))));
   event.waitUntil(clients.claim());
   postMessageAllClients({ action: "service-worker-activated" });
@@ -8,7 +9,7 @@ self.addEventListener("activate",event => {
 self.addEventListener("fetch",event => {
   event.respondWith(caches.match(event.request).then(response => {
     return response || fetch(event.request).then(async response => {
-      caches.open(1.1).then(cache => cache.put(event.request,response));
+      caches.open(version).then(cache => cache.put(event.request,response));
       return response.clone();
     });
   }));
