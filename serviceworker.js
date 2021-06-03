9c3ac40fb7c71bb5ba67bcd1164636b71d53049b
xJustJqy <78117567+xJustJqy@users.noreply.github.com>
2021-06-03 10:29:37 -0700

    Added basic service worker

---
+++ serviceworker.js
@@ -0,0 +1,23 @@
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/manifest.webmanifest',
+       '/resources/icon-256.png'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
